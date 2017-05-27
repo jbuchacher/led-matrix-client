8cbcdc3dbe03ca6d77032d93d1f9af62f1456191
Joshua Buchacher <jbuchacher@me.com>
2017-05-26 21:21:01 -1000

    New Xcode project

---
+++ led-matrix-clientTests/led_matrix_clientTests.swift
@@ -0,0 +1,36 @@
+//
+//  led_matrix_clientTests.swift
+//  led-matrix-clientTests
+//
+//  Created by Josh Buchacher on 5/26/17.
+//  Copyright © 2017 Josh Buchacher. All rights reserved.
+//
+
+import XCTest
+@testable import led_matrix_client
+
+class led_matrix_clientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
